81f8b34eb26ddc426d8f317d2a0ee26a479249e0
Kieran B <62178721+kb1107@users.noreply.github.com>
2021-06-22 16:51:57 +0100

    User entry boxes and buttons added to main window

--- script1.py
+++ script1.py
@@ -32,5 +32,34 @@ isbn_text = StringVar()
 ent_isbn = Entry(window, textvariable=isbn_text)
 ent_isbn.grid(row=1, column=3)
 
+#list box & scrollbar
+lb_output = Listbox(window, height=6, width=35)
+lb_output.grid(row=2, column=0, rowspan=6, columnspan=2)
+
+sb1 = Scrollbar(window)
+sb1.grid(row=2, column=2, rowspan=6)
+
+lb_output.configure(yscrollcommand=sb1.set)
+sb1.configure(command=lb_output.yview)
+
+#buttons
+btn_view = Button(window, text="View All", width=12)
+btn_view.grid(row=2, column=3)
+
+btn_search = Button(window, text="Search Entry", width=12)
+btn_search.grid(row=3, column=3)
+
+btn_add = Button(window, text="Add Entry", width=12)
+btn_add.grid(row=4, column=3)
+
+btn_update = Button(window, text="Update", width=12)
+btn_update.grid(row=5, column=3)
+
+btn_delete = Button(window, text="Delete", width=12)
+btn_delete.grid(row=6, column=3)
+
+btn_close = Button(window, text="Close", width=12)
+btn_close.grid(row=7, column=3)
+
 window.mainloop()
 
