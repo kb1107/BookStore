93813197b4e3a3448472b7d2d030fcd229e46b9a
Kieran B <62178721+kb1107@users.noreply.github.com>
2021-06-22 16:03:11 +0100

    first commit

---
+++ script1.py
@@ -0,0 +1,15 @@
+"""
+This program stores the following information about books:
+Title, Author
+Year, ISBN
+
+The user can:
+
+View all records
+Search an entry
+Add an entry
+Update an entry
+Delete an entry
+Close the program
+"""
+
