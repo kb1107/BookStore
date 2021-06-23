2fe7234f4688e17f3dddc827f58f53acd3c279a5
kb1107 <kieran.brady123@gmail.com>
2021-06-23 14:27:11 +0100

    New file added 'backend.py' and script1 renamed to frontend.

---
+++ backend.py
@@ -0,0 +1,9 @@
+import sqlite3
+
+def connect():
+    conn=sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS book(id INTEGER PRIMARY KEY, title text, author TEXT, year INTEGER, isbn INTEGER)")
+    conn.commit()
+    conn.close()
+
